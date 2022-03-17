4983cefea25f49493ba3dd86315cc64ac9ef83db
Buntaro.Orita <buntaro.orita@gmail.com>
2022-03-17 09:56:51 +0900

    Inspect PNG file format

--- chap03_ioreader/main.go
+++ chap03_ioreader/main.go
@@ -6,10 +6,9 @@ import (
 	"fmt"
 	"io"
 	"log"
-	"strings"
-
 	"net"
 	"os"
+	"strings"
 )
 
 func main() {
@@ -19,8 +18,9 @@ func main() {
 	// buf()
 	// limit()
 	// section()
+	// convertEndian()
 
-	convertEndian()
+	inspectPNG()
 }
 
 // 3.4.1 os.Stdin
@@ -94,12 +94,61 @@ func section() {
 	io.Copy(os.Stdout, sectionR) // => Section
 }
 
+// 3.5.2 binary.Read
 func convertEndian() {
-
 	data := []byte{0x0, 0x0, 0x27, 0x10} // 10000 (32bits big endian)
 	var i int32
 
 	// 実行環境のエンディアンの数値に変換する
 	binary.Read(bytes.NewReader(data), binary.BigEndian, &i)
 	fmt.Println(i) // => 10000 (little endian for Intel CPU)
+}
+
+// 3.5.3
+func inspectPNG() {
+	file, err := os.Open("img/lenna.png")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer file.Close()
+
+	chunks := readPNGChunks(file)
+	for _, chunk := range chunks {
+		dumpPNGChunk(chunk)
+	}
+}
+
+// A PNG file is composed of an 8-byte signature header and chunks.
+// Each chunk contains 4 fields
+// * (4-byte) length of data
+// * (4-byte) type
+// * (length bytes) data
+// * (4-byte) CRC
+func readPNGChunks(file *os.File) []io.Reader {
+	var chunks []io.Reader
+
+	file.Seek(8, 0) // skip signature header
+	var offset int64 = 8
+	for {
+		var length int32 // 4-byte
+		err := binary.Read(file, binary.BigEndian, &length)
+		if err == io.EOF {
+			break
+		}
+		chunks = append(chunks,
+			io.NewSectionReader(file, offset, int64(length)+12)) // 4+4+length+4
+
+		// move to head of next chunk()
+		// 4(type)+lenght(data)+4(CRC)
+		offset, _ = file.Seek(int64(length+8), 1)
+	}
+	return chunks
+}
+
+func dumpPNGChunk(chunk io.Reader) {
+	var length int32 // 4-byte for length
+	binary.Read(chunk, binary.BigEndian, &length)
+	typ := make([]byte, 4) // 4-byte for type
+	chunk.Read(typ)
+	fmt.Printf("chunk %q (%d bytes)\n", string(typ), length)
 }
